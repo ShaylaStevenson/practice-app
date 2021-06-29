eb54fc9ba22db167db61ef3c746b539256a1b112
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-06-29 14:23:27 -0700

    deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import profilePicture from "./img/profilePicture.jpg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Practice App</h1>
+      <h2>Author: Shayla Stevenson</h2>
+      <img src={profilePicture} alt="Shayla Stevenson" />
     </div>
   );
 }
